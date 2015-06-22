1f9d287ad4aa11a99743b22babed026be6373e3a
franklindeoliveira <franklindeoliveira@gmail.com>
2015-06-22 15:12:45 -0300

    Definindo um header, content e footer para o layout da application.hbs.

--- app/routes/favorite-view.js
+++ app/routes/favorite-view.js
@@ -1,11 +1,1 @@
-export default Ember.Route.extend({
-
-  // model: function(params) {
-  // 	return [
-  // 		{id: 0, name: 'Item 0'},
-  // 		{id: 1, name: 'Item 1'},
-  // 		{id: 2, name: 'Item 2'}
-  // 	].get(params.id);
-  // }
-
-});
+export default Ember.Route.extend();
